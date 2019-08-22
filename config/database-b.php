5ff7548b59122f6732e02715ba6d1853e1a1769a
git <naz@z>
2019-08-22 10:33:23 +0800

    addconfig

--- config/database-b.php
+++ config/database-b.php
@@ -44,7 +44,7 @@ return [
             'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', ''),
-            'username' => env('DB_USERNAME', ''),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
